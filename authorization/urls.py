2f4efd6b883608f5b74e79f36a9f8d65fa210ef9
gmike99 <gursky.michael1999@gmail.com>
2018-11-01 01:24:32 +0200

    added token retrieval

--- authorization/urls.py
+++ authorization/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('login/', views.SimpleUserLogin.as_view(template_name='rest_framework/login.html'), name='login'),
     path('logout/', views.SimpleUserLogout.as_view(), name='logout'),
+    path('rest-auth/', include('rest_auth.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
